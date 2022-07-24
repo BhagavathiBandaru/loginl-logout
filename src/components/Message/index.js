72fffaa28c3a7ac649037029c61f0c840e5d29e3
Bhagavathi <bhagavathibandaru0@gmail.com>
2022-07-24 09:37:01 +0000

    loginlogout

---
+++ src/components/Message/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+const Message = props => {
+  const {isLogin} = props
+  const displayMessageText = isLogin ? 'Welcome User' : 'Please Login'
+
+  return <h1 className="display-message">{displayMessageText}</h1>
+}
+
+export default Message
